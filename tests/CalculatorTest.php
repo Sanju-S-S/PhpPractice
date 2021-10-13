c664518344129e78fa0d1fb1c5e2b56d86d36c82
Sanju <49448546+Sanju-S-S@users.noreply.github.com>
2021-10-13 11:04:17 +0530

    PhpUnit sample tests with results

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require 'calculator.php';
+ 
+class CalculatorTest extends TestCase
+{
+    private $calculator;
+ 
+    protected function setUp() : void
+    { 
+        $this->calculator = new Calculator();
+    }
+ 
+    protected function tearDown() :void
+    {
+        $this->calculator = NULL;
+    }
+ 
+    public function testAdd()
+    {
+        $result = $this->calculator->add(1, 2);
+        $this->assertEquals(3, $result);
+    }
+ 
+}
